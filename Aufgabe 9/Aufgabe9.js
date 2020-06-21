9406a38f03ca001cf75b30b1a7639f134f6946a4
mariehoffm <64094445+mariehoffm@users.noreply.github.com>
2020-06-21 13:59:50 +0200

    g

--- Aufgabe 9/Aufgabe9.js
+++ Aufgabe 9/Aufgabe9.js
@@ -1,13 +1,23 @@
 var TaskList = ["task1", "task2"];
+document.getElementById("ausgabe").innerHTML = TaskList; // geht nicht.....
 //Array soll als Liste auf der Webseite zu sehen sein
 //document.getElementById("dieliste").innerHTML = TaskList; //geht nicht weil Listenlänge überschreibt, ist außerdem keine Liste sondern Aufzählung
 //console.log(input.value) //geht nicht omg why
-document.querySelector("#addTaskButton").addEventListener("click", function () { ListeBitte(); });
-function ListeBitte() {
-    var x = document.getElementById("input").value;
-    document.getElementById("ausgabe").innerHTML = x;
+document.querySelector("button").addEventListener("click", myFunctionListe);
+function myFunctionListe() {
+    //alert("test"); //--> funktioniert !!
+    var task = document.getElementById("input").value;
+    alert(task); // --> funktioniert auch omg
+    //jetzt hinten ans array dran:
+    TaskList.push('testttt'); //WHY???? macht no sinn
 }
 /* GEHT ALLES NICHT:
+
+  function ListeBitte() {
+    var x = document.getElementById("input").value;
+    document.getElementById("ausgabe").innerHTML = x;
+  }
+
 
 function ListeBitte(){
     if (document.form.input.value != "") {
--- Aufgabe 9/Aufgabe9.ts
+++ Aufgabe 9/Aufgabe9.ts
@@ -1,21 +1,31 @@
 
 
-let TaskList: string [] = ["task1", "task2"];
+const TaskList: string [] = ["task1", "task2"];
+
+document.getElementById("ausgabe").innerHTML = TaskList; // geht nicht.....
+
 
 //Array soll als Liste auf der Webseite zu sehen sein
 //document.getElementById("dieliste").innerHTML = TaskList; //geht nicht weil Listenlänge überschreibt, ist außerdem keine Liste sondern Aufzählung
 //console.log(input.value) //geht nicht omg why
 
-document.querySelector("#addTaskButton").addEventListener("click", function() {ListeBitte();});
+document.querySelector("button").addEventListener("click", myFunctionListe);
 
+function myFunctionListe(){
+    //alert("test"); //--> funktioniert !!
+    let task = document.getElementById("input").value;
+    alert(task); // --> funktioniert auch omg
+    //jetzt hinten ans array dran:
+    TaskList.push('testttt'); //WHY???? macht no sinn
+}
 
+/* GEHT ALLES NICHT:
 
   function ListeBitte() {
     var x = document.getElementById("input").value;
     document.getElementById("ausgabe").innerHTML = x;
   }
 
-/* GEHT ALLES NICHT:
 
 function ListeBitte(){
     if (document.form.input.value != "") {

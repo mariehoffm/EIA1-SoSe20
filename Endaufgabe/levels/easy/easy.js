62393c4382391c562d7faf4d12072e86129a651e
mariehoffm <64094445+mariehoffm@users.noreply.github.com>
2020-07-13 14:14:46 +0200

    wg

--- Endaufgabe/levels/easy/easy.js
+++ Endaufgabe/levels/easy/easy.js
@@ -1,43 +1,41 @@
 //erst Variable mit allen Tönen deklarieren:
-var sound = ["/sounds/sound_boing.mp3", "/sounds/sound_boing_2.mp3", "/sounds/sound_kids_booo.mp3", "/sounds/sound_laugh.mp3"];
+var sound = ["sound_boing.mp3", "/sounds/sound_boing_2.mp3", "/sounds/sound_kids_booo.mp3", "/sounds/sound_laugh.mp3", "/sounds/sound_squeaking.mp3"];
+//sounds die geclickt (& gespielt) werden in einem array speichern
+var playedSounds = [];
 //Ton soll abgespielt werden wenn auf den entsprechenden button geklickt wird
 //erster Ton - boing
 document.getElementById("button1").addEventListener("click", function () {
     var playsound = new Audio(sound[0]);
     playsound.play();
-    playedSounds.push();
+    playedSounds.push("/sounds/sound_boing.mp3");
 });
 //zweiter Ton - boing 2
 document.getElementById("button2").addEventListener("click", function () {
     var playsound = new Audio(sound[1]);
     playsound.play();
-    playedSounds.push();
+    //window.alert(sound[1]);
+    playedSounds.push(playsound.src);
 });
-//zweiter Ton - kids booo
+//dritter Ton - kids booo
 document.getElementById("button3").addEventListener("click", function () {
     var playsound = new Audio(sound[2]);
     playsound.play();
-    playedSounds.push();
+    playedSounds.push(sound[2]);
 });
-//zweiter Ton - laugh
+//vierter Ton - laugh
 document.getElementById("button4").addEventListener("click", function () {
     var playsound = new Audio(sound[3]);
     playsound.play();
-    playedSounds.push();
+    playedSounds.push(sound[3]);
 });
-//sounds die geclickt (&gespielt) werden in einem array speichern
-var playedSounds = [];
+//fünfter Ton - squeaking
+document.getElementById("button5").addEventListener("click", function () {
+    var playsound = new Audio(sound[4]);
+    playsound.play();
+    playedSounds.push(sound[4]);
+});
+//gespielte Töne in der Konsole ausgeben:
 console.log(playedSounds);
-function soundEingabe() {
-    var x = 0;
-    //window.alert(anzahl);
-    setInterval(function () {
-        var eingabeSong = new Audio(playedSounds[x]);
-        eingabeSong.play();
-        console.log(playedSounds[x]);
-        x++; //i=i+1
-    }, 500);
-}
 //array festlegen mit 5 random tönen --> später random machen
 var song = ["/sounds/sound_boing_2.mp3", "/sounds/sound_boing_2.mp3", "/sounds/sound_boing.mp3", "/sounds/sound_laugh.mp3", "/sounds/sound_laugh.mp3"];
 //gespielte Töne mit randomArray vergleichen --> richtig ? weiter / falsch? youLoose / nach 5 TÖnen immernoch richtig? youWin
--- Endaufgabe/levels/easy/easy.ts
+++ Endaufgabe/levels/easy/easy.ts
@@ -1,53 +1,49 @@
 
 //erst Variable mit allen Tönen deklarieren:
-var sound: string[] =["/sounds/sound_boing.mp3", "/sounds/sound_boing_2.mp3", "/sounds/sound_kids_booo.mp3", "/sounds/sound_laugh.mp3"];
+var sound: string[] =["sound_boing.mp3", "/sounds/sound_boing_2.mp3", "/sounds/sound_kids_booo.mp3", "/sounds/sound_laugh.mp3", "/sounds/sound_squeaking.mp3"];
+
+//sounds die geclickt (& gespielt) werden in einem array speichern
+var playedSounds: string[] = [];
 
 //Ton soll abgespielt werden wenn auf den entsprechenden button geklickt wird
 //erster Ton - boing
 document.getElementById("button1").addEventListener("click", function (){
-    var playsound = new Audio(sound[0]); 
+    var playsound: HTMLAudioElement = new Audio(sound[0]); 
     playsound.play();
-    playedSounds.push();
+    playedSounds.push("/sounds/sound_boing.mp3");
 });
 
 //zweiter Ton - boing 2
 document.getElementById("button2").addEventListener("click", function (){
-    var playsound = new Audio(sound[1]); 
+    var playsound: HTMLAudioElement = new Audio(sound[1]); 
     playsound.play();
-    playedSounds.push();
+    //window.alert(sound[1]);
+    playedSounds.push(playsound.src);
 });
 
-//zweiter Ton - kids booo
+//dritter Ton - kids booo
 document.getElementById("button3").addEventListener("click", function (){
-    var playsound = new Audio(sound[2]); 
+    var playsound: HTMLAudioElement = new Audio(sound[2]); 
     playsound.play();
-    playedSounds.push();
+    playedSounds.push(sound[2]);
 });
 
-//zweiter Ton - laugh
+//vierter Ton - laugh
 document.getElementById("button4").addEventListener("click", function (){
-    var playsound = new Audio(sound[3]); 
+    var playsound: HTMLAudioElement = new Audio(sound[3]); 
     playsound.play();
-    playedSounds.push();
+    playedSounds.push(sound[3]);
 });
 
+//fünfter Ton - squeaking
+document.getElementById("button5").addEventListener("click", function (){
+    var playsound: HTMLAudioElement = new Audio(sound[4]); 
+    playsound.play();
+    playedSounds.push(sound[4]);
+});
 
-
-//sounds die geclickt (&gespielt) werden in einem array speichern
-var playedSounds: string[] = []
+//gespielte Töne in der Konsole ausgeben:
 console.log(playedSounds);
-
-function soundEingabe(){
-    var x: number = 0;
-	//window.alert(anzahl);
-	setInterval(function() {
-		var eingabeSong = new Audio(playedSounds[x]);
-		eingabeSong.play();
-		console.log(playedSounds[x]);
-		x++; //i=i+1
-	}, 500); 
-}
-
 
 
 

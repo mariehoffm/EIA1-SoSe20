b11982013f9cb59c6be2009d63003ae439e0c151
mariehoffm <64094445+mariehoffm@users.noreply.github.com>
2020-07-14 12:49:55 +0200

    we

--- Endaufgabe/levels/easy/easy.js
+++ Endaufgabe/levels/easy/easy.js
@@ -1,18 +1,16 @@
-//erst Variable mit allen Tönen deklarieren:
-var sound = ["sound_boing.mp3", "sound_boing_2.mp3", "sound_kids_booo.mp3", "sound_laugh.mp3", "sound_squeaking.mp3"];
 //sounds die geclickt (& gespielt) werden in einem array speichern
 var playedSounds = [];
 //Ton soll abgespielt werden wenn auf den entsprechenden button geklickt wird
 //erster Ton - boing
 document.getElementById("button1").addEventListener("click", function () {
-    var playsound = new Audio(sound[0]);
+    var playsound = new Audio("sound_boing.mp3");
     playsound.play();
     playedSounds.push(sound[1]);
     console.log("playedSoundArray: ", playedSounds); //gespielte Töne in der Konsole ausgeben
 });
 //zweiter Ton - boing 2
 document.getElementById("button2").addEventListener("click", function () {
-    var playsound = new Audio(sound[1]);
+    var playsound = new Audio("sound_boing_2.mp3");
     playsound.play();
     //window.alert(sound[1]);
     playedSounds.push(sound[1]);
@@ -20,26 +18,36 @@ document.getElementById("button2").addEventListener("click", function () {
 });
 //dritter Ton - kids booo
 document.getElementById("button3").addEventListener("click", function () {
-    var playsound = new Audio(sound[2]);
+    var playsound = new Audio("sound_kids_booo.mp3");
     playsound.play();
     playedSounds.push(sound[2]);
     console.log("playedSoundArray: ", playedSounds); //gespielte Töne in der Konsole ausgeben
 });
 //vierter Ton - laugh
 document.getElementById("button4").addEventListener("click", function () {
-    var playsound = new Audio(sound[3]);
+    var playsound = new Audio("sound_laugh.mp3");
     playsound.play();
     playedSounds.push(sound[3]);
     console.log("playedSoundArray: ", playedSounds); //gespielte Töne in der Konsole ausgeben
 });
 //fünfter Ton - squeaking
 document.getElementById("button5").addEventListener("click", function () {
-    var playsound = new Audio(sound[4]);
+    var playsound = new Audio("sound_squeaking.mp3");
     playsound.play();
     playedSounds.push(sound[4]);
     console.log("playedSoundArray: ", playedSounds); //gespielte Töne in der Konsole ausgeben
 });
 //array festlegen mit 5 random tönen --> später random machen
 var song = ["/sounds/sound_boing_2.mp3", "/sounds/sound_boing_2.mp3", "/sounds/sound_boing.mp3", "/sounds/sound_laugh.mp3", "/sounds/sound_laugh.mp3"];
+window.addEventListener('load', function () {
+    var playsong = new Audio(song[0]);
+    playsong.play();
+    if (song[0] == playedSounds[0]) { // wenn der gleiche ton gespielt wurde wiederholen
+        var ö = 0;
+        var playsong2 = new Audio(song[ö]);
+        playsong2.play();
+        ++ö;
+    } // funktioniert NOCH nicht
+});
 //gespielte Töne mit randomArray vergleichen --> richtig ? weiter / falsch? youLoose / nach 5 TÖnen immernoch richtig? youWin
 //# sourceMappingURL=easy.js.map
--- Endaufgabe/levels/easy/easy.ts
+++ Endaufgabe/levels/easy/easy.ts
@@ -1,24 +1,19 @@
 
-//erst Variable mit allen Tönen deklarieren:
-var sound: string[] =["sound_boing.mp3", "sound_boing_2.mp3", "sound_kids_booo.mp3", "sound_laugh.mp3", "sound_squeaking.mp3"];
-
 //sounds die geclickt (& gespielt) werden in einem array speichern
 var playedSounds: string[] = [];
 
 //Ton soll abgespielt werden wenn auf den entsprechenden button geklickt wird
 //erster Ton - boing
 document.getElementById("button1").addEventListener("click", function (){
-    var playsound: HTMLAudioElement = new Audio(sound[0]); 
+    var playsound: HTMLAudioElement = new Audio("sound_boing.mp3"); 
     playsound.play();
     playedSounds.push(sound[1]);
     console.log("playedSoundArray: "playedSounds);//gespielte Töne in der Konsole ausgeben
-
-
 });
 
 //zweiter Ton - boing 2
 document.getElementById("button2").addEventListener("click", function (){
-    var playsound: HTMLAudioElement = new Audio(sound[1]); 
+    var playsound: HTMLAudioElement = new Audio("sound_boing_2.mp3"); 
     playsound.play();
     //window.alert(sound[1]);
     playedSounds.push(sound[1]);
@@ -29,7 +24,7 @@ document.getElementById("button2").addEventListener("click", function (){
 
 //dritter Ton - kids booo
 document.getElementById("button3").addEventListener("click", function (){
-    var playsound: HTMLAudioElement = new Audio(sound[2]); 
+    var playsound: HTMLAudioElement = new Audio("sound_kids_booo.mp3"); 
     playsound.play();
     playedSounds.push(sound[2]);
     console.log("playedSoundArray: "playedSounds);//gespielte Töne in der Konsole ausgeben
@@ -39,7 +34,7 @@ document.getElementById("button3").addEventListener("click", function (){
 
 //vierter Ton - laugh
 document.getElementById("button4").addEventListener("click", function (){
-    var playsound: HTMLAudioElement = new Audio(sound[3]); 
+    var playsound: HTMLAudioElement = new Audio("sound_laugh.mp3"); 
     playsound.play();
     playedSounds.push(sound[3]);
     console.log("playedSoundArray: "playedSounds);//gespielte Töne in der Konsole ausgeben
@@ -49,7 +44,7 @@ document.getElementById("button4").addEventListener("click", function (){
 
 //fünfter Ton - squeaking
 document.getElementById("button5").addEventListener("click", function (){
-    var playsound: HTMLAudioElement = new Audio(sound[4]); 
+    var playsound: HTMLAudioElement = new Audio("sound_squeaking.mp3"); 
     playsound.play();
     playedSounds.push(sound[4]);
     console.log("playedSoundArray: "playedSounds);//gespielte Töne in der Konsole ausgeben
@@ -62,14 +57,21 @@ document.getElementById("button5").addEventListener("click", function (){
 //array festlegen mit 5 random tönen --> später random machen
 var song: string[] = [ "/sounds/sound_boing_2.mp3",  "/sounds/sound_boing_2.mp3", "/sounds/sound_boing.mp3", "/sounds/sound_laugh.mp3", "/sounds/sound_laugh.mp3"]
 
+window.addEventListener('load', function() {
+        var playsong = new Audio(song[0]);
+        playsong.play();
+
+        if (song[0] == playedSounds[0]){// wenn der gleiche ton gespielt wurde wiederholen
+            var ö = 0;
+            var playsong2 = new Audio (song[ö]);
+            playsong2.play();
+            ++ö;
+        } // funktioniert NOCH nicht
+});
+
 
 
 
 //gespielte Töne mit randomArray vergleichen --> richtig ? weiter / falsch? youLoose / nach 5 TÖnen immernoch richtig? youWin
-
-
-
-
-
 
 
